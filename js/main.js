9835acf4a86077774cbfa9ca149fceb6da179689
MissDitch <mjtsloot@icloud.com>
2017-05-19 13:01:00 +0200

    resume and pause working, also alternating session/break

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ function init() {
     updateClock("25");
 }
 
+var pomodoro, state = 0; //  0 = idle, 1 = running, 2 = paused, 3= resumed
+var isRunning = false;
+var isSession = true;
+var intervalId;
+
 function addListener(prop, selector, func) {
     if (prop === "class") {
         var sels = document.querySelectorAll(selector);
@@ -16,8 +21,7 @@ function addListener(prop, selector, func) {
     if (prop === "id") {
         el = document.getElementById(selector);
         el.addEventListener("click", func);
-    }
-    
+    }    
 }
 
 function add(e) {
@@ -41,8 +45,8 @@ function subtract(e) {
 }
 
 function updateClock(val) {    
-    var el = document.getElementById("time");
-    el.textContent = val + ":00";    
+    var time= document.getElementById("time");
+    time.textContent = val + ":00";    
 }
 
 function reset() {
@@ -58,49 +62,97 @@ function reset() {
     message.textContent = "";
 }
 
-var isRunning = false;
-var isSession = true;
-var intervalId;
-var state = 0; // initial
-
-function play(isSession) {
-    state = 1;
-     var minute, second, display, message;
-     message = document.getElementById("message");
-  //   if (state === 0) {    
-    if (isSession) {  //clock.state === session
-        display = document.getElementById("workTime");  
-        message.textContent = "All Work and no play";      
-    } else {
-        display = document.getElementById("breakTime");
-        message.textContent = "Gimme a break!";
+function toggleStart(e) {
+    var control = document.getElementById("play_pause");
+    if (!isRunning) {        
+        control.setAttribute("class", "fa fa-pause"); // show pause icon
+        // changing session or break length is not possible        
+        // start or resume timer
+        if (state === 0) {
+            pomodoro = new Pomodoro(); //callback, 1000
+            state = 1;
+            pomodoro.start();
+        }  
+        if (state === 2) {
+            state = 3;
+            pomodoro.resume();
+        }                
+        isRunning = true;        
+    } else {              
+        control.setAttribute("class", "fa fa-play"); // show play icon 
+        // changing session or break length is possible?
+        if (state === 1 || state === 3) {
+            pomodoro.pause();
+        }  
+        isRunning = false;  
     }
-    minute = display.textContent;
-    //minute = 1;
-    minute--;
-    second = "59"; 
-    start(isSession, minute, second);  
-  //  } 
-  //  if (state > 1) {
-  //  }
 }
 
-function start(isSession, minute, second) {
+function Pomodoro() {  //callback, interval
+    var minute, second, intervalId, startTime, remaining = 0;
+    var timeString, time = document.getElementById("time");
+
+    this.start = function() {
+        play(isSession, 0, minute, second);
+    };
+    this.pause = function () {        
+        timeString = time.textContent;
+        minute = timeString.slice(0,2);
+        second = timeString.slice(3);
+        state = 2;
+        pause();        
+    };
+    this.resume = function () {
+        second--;
+        play(isSession, 2, minute, second);
+    };    
+}
+
+function play(isSession, state, minute, second) {
+    var display, message, time, minute, second;
+    if (arguments.length > 2) {
+        minute = minute;
+        second = second;
+    }
+    message = document.getElementById("message");
+    time = document.getElementById("time");
+  
+    if (state === 0) {    
+        state = 1;
+        if (isSession) {  
+            display = document.getElementById("workTime");  
+            message.textContent = "All Work and no play";      
+        } else {
+            display = document.getElementById("breakTime");
+            message.textContent = "Gimme a break!";
+        }
+        minute = display.textContent;
+        minute--;
+        second = "59";  
+        run(isSession, minute, second);      
+    } 
+    if (state === 2) {
+        state = 3; 
+        run(isSession, minute, second);
+    }      
+}
+
+function run (isSession, minute, second) {
     clearInterval(intervalId);   
-    var el = document.getElementById("time");  
+    var time = document.getElementById("time");  
     intervalId = setInterval(function() {
         console.log(minute + ", " + second);
         second < 10 && second >= 0 ? second = "0"+ second : second = second;
         minute < 10 && minute.toString().length === 1 ? minute = "0"+ minute : minute = minute;           
-        el.textContent = minute + ":" + second;
+        time.textContent = minute + ":" + second;
         if (minute === '00' && second === '00') { 
             if (isSession) {
                 isSession = false; 
-                play(isSession);                  
+                play(isSession, 0);                  
             } 
             else {
                 isSession = true;
-                play(isSession);
+                play(isSession, 0);
             }
         }
         if (second === '00') {
@@ -111,75 +163,10 @@ function start(isSession, minute, second) {
     }, 1000);
 }
 
-function resume() {
-
-}
-
 function pause() {
-           clearInterval(intervalId);
-        }
-
-function toggleStart(e) {
-    var control = document.getElementById("play_pause");
-    if (!isRunning) {        
-        control.setAttribute("class", "fa fa-pause"); // show pause icon
-        // changing session or break length is not possible        
-        // start or resume timer
-        if (state === 0) {
-            play(isSession); 
-        }   
-        if (state === 1) {
-            resume();
-        }         
-        isRunning = true;        
-    } else {              
-        control.setAttribute("class", "fa fa-play"); // show play icon 
-        // changing session or break length is possible
-        pause();  // pause timer
-        isRunning = false;  
-    }
-    // depending on state, run function:
-
-    // if clock is not running:     
-    // if clock is in play state
-    // if clock is in pause state
+   clearInterval(intervalId);
 }
 
 
-
-// clock has state session or break, do i also need a neutral state? 
-
-// gets time from controls display
-function getTime() {
-    var minute, second, display;
-    if (isSession) {  //clock.state === session
-        display = document.getElementById("workTime");        
-    } else {
-        display = document.getElementById("breakTime");
-    }
-    minute = display.textContent;
-    second = "00";
-
-}
-// shows time running on clock
-function run(minute, second) {
-    var minute = minute;
-    var second = second;
-    var el = document.getElementById("time");
-  
-    var intervalId = setInterval(function() {
-        console.log("is running!");
-        el.textContent = minute + ":" + second;
-        if (minute === 0 && second === 0) {return;}
-        if (minute !== 0 && second === 0) {
-            minute--;
-            second === 60;
-        }
-        // decrease seconds and /or minutes 
-        second--;
-        
-    }, 1000);
-
-}
 
 init();
